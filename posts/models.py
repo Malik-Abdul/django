c1784019a9b1471836820ff793751ce3e2eca88d
Abdul Ghafoor <abdul.ghafoor@dubizzlelabs.com>
2024-12-27 03:25:22 +0500

    Migrations and models

--- posts/models.py
+++ posts/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+
+class Post(models.Model):
+    title = models.CharField(max_length=75) # explore: https://docs.djangoproject.com/en/5.1/topics/db/models/#field-types
+    body = models.TextField()
+    slug = models.SlugField
+    date = models.DateTimeField(auto_now_add=True) # will automatically date time stamp when user add data in this table
+
+    def __str__(self):
+        return self.title
